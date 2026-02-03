39f54bf0bbaf1ed33d5e2cbe14fa0b51d5666b4b
antonioroserom <wrosero9059@utm.edu.ec>
2026-02-03 13:50:34 -0500

    Inserto código para establecer la conexión con la base de datos

---
+++ src/config/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = $_ENV['DB_HOST'] ?? 'localhost';
+$dbname = $_ENV['DB_NAME'] ?? 'prestamistapp';
+$user = $_ENV['DB_USER'] ?? 'root';
+$pass = $_ENV['DB_PASS'] ?? '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
